58bf3762e137787f657b9ee86b46bce571d33722
Graig Janssen <graig@Graigs-iMac.local>
2021-11-17 22:26:41 -0500

    Copy edits, remove form logic, add meta desc

--- scripts.js
+++ scripts.js
@@ -10,44 +10,14 @@ function emailValidation() {
   if (mail1 !== mail2) {
     errorMessage.classList.remove('invisible');
     errorMessage.setAttribute('autofocus', '');
-    return false;
   } else {
     errorMessage.classList.add('invisible');
-    return true;
   }
 }
 
 function handleSubmit(e) {
   e.preventDefault();
-  if (emailValidation()) {
-    const reqBody = generateRequestBody();
-    console.log('reqBody: ', reqBody);
-  }
-}
-
-function generateRequestBody() {
-  const reqBody = {
-    selectedService: null,
-    selectedCities: [],
-  };
-  const elements = form.elements;
-  for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-    if (element.type !== 'submit') {
-      if (element.type === 'radio') {
-        if (element.checked) {
-          reqBody.selectedService = element.value;
-        }
-      } else if (element.type === 'checkbox') {
-        if (element.checked) {
-          reqBody.selectedCities.push(element.value);
-        }
-      } else {
-        reqBody[element.id] = element.value;
-      }
-    }
-  }
-  return reqBody;
+  emailValidation();
 }
 
 function init() {
